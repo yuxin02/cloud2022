d4483d778aeecdc153a27e29a512fb05c31310c8
yuxin <yuxin02@gmail.com>
2022-07-24 11:07:23 +0800

    add nacos support

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wiseco/AlibabaConfigNacosMain3377.java
@@ -0,0 +1,14 @@
+package com.wiseco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlibabaConfigNacosMain3377 {
+    public static void main(String[] args){
+        SpringApplication.run(AlibabaConfigNacosMain3377.class, args);
+        System.out.println("启动成功");
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/wiseco/AlibabaConsumerMain83.java
@@ -0,0 +1,12 @@
+package com.wiseco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaConsumerMain83 {
+    public static void main(String[] args){
+        SpringApplication.run(AlibabaConsumerMain83.class, args);
+        System.out.println("启动成功");
+    }
+}
